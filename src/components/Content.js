e7eef258d00135d8f51d416143ab911be7e9b3bc
leshast683 <147610263+leshast683@users.noreply.github.com>
2025-02-04 16:17:13 -0500

    Initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <main>
+            <p>Once upon a time, there was a hare who boasted about how fast he could run. Tired of hearing him boast, the tortoise challenged him to a race.</p>
+            <p>All the animals in the forest gathered to watch. The hare ran down the road for a while and then paused to rest. He looked back at the tortoise and cried out, "How do you expect to win this race when you are walking along at your slow, slow pace?"</p>
+            <p>The hare stretched himself out alongside the road and fell asleep, thinking, "There is plenty of time to relax." The tortoise walked and walked, never ever stopping until he came to the finish line. The animals who were watching cheered so loudly for tortoise that they woke up the hare. The hare stretched, yawned and began to run again, but it was too late. The tortoise had already crossed the finish line.</p>
+        </main>
+    );
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Moral: Slow and steady wins the race.</p>
+        </footer>
+    );
+};
+
+export default Footer;
